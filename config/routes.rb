f289d34f8b5c7c4f6458ab317be9b082ce4095e0
nemi11 <nemi_ra_men@yahoo.co.jp>
2025-09-13 22:04:13 +0900

    Update README_3.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
-# frozen_string_literal: true
-
+# config/routes.rb
 Rails.application.routes.draw do
   get 'pages/home'
-
   root 'pages#home'
 
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+
+  # ログイン用ルート
+  get    'login',  to: 'sessions#new'     # ログインフォーム
+  post   'login',  to: 'sessions#create'  # ログイン処理
+  delete 'logout', to: 'sessions#destroy' # ログアウト
 
   resources :users, only: %i[new create]
 end
