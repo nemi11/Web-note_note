5c5804f3774ba508b7a790da5eb83de890d5d261
nemi11 <nemi_ra_men@yahoo.co.jp>
2025-10-20 21:59:51 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  email: 'admin@example.com',
+  password: 'password123',
+  password_confirmation: 'password123',
+  admin: true
+)
