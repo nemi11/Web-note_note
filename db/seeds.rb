7ed14b74bb24b58b99e269fb5ca2550395e65933
nemi11 <nemi_ra_men@yahoo.co.jp>
2025-10-20 22:15:01 +0900

    Add confirmable fields to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,5 @@ User.find_or_create_by!(email: 'admin@example.com') do |user|
   user.password = 'password123'
   user.password_confirmation = 'password123'
   user.admin = true
+  user.confirmed_at = Time.current # ← これで確認済み扱いになる
 end
